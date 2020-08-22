7bbd597aac60eaeeb7bad20f49a6c84f378c2604
redmindtech <69637152+redmindtech@users.noreply.github.com>
2020-08-22 19:38:36 +0530

    Update Food_web.py

--- Food_web.py
+++ Food_web.py
@@ -12,7 +12,7 @@ from flask import jsonify
 import course
 
 app = Flask(__name__)
-mydb = mysql.connector.connect(host="127.0.0.1", user="root", passwd="", database="dt")
+mydb = mysql.connector.connect(host="127.0.0.1", user="root", passwd="", database="dt",port="3306")
 mycursor = mydb.cursor()
 app.secret_key=0
 app.secret_code=1
@@ -21,7 +21,7 @@ app.secret_code=1
 def food():
     try:
         invoke_next_question = True
-        dbcon = mysql.connector.connect(host="127.0.0.1", user="root", password="", database="dt_mdle")
+        dbcon = mysql.connector.connect(host="127.0.0.1", user="root", password="", database="dt_mdle",port="3306")
         req = request.get_json(silent=True, force=True)
 
         print("----------------START-------------------")
