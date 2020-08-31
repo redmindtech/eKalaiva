e64cbc5a1fd88c74572dc9f7ba78f6d1aa9fd367
redmindtech <69637152+redmindtech@users.noreply.github.com>
2020-08-31 12:13:05 +0530

    Update Food_web.py

--- Food_web.py
+++ Food_web.py
@@ -39,6 +39,8 @@ def food():
         print("current_intent - ", current_intent)
         print("current_reply - ", current_reply)
         
+        emailid = req.get("queryResult")
+        print("emailid--",emailid)
         emailid = req.get("queryResult").get("querytext")
         #value = (req.get('queryResult'))
         #name = value.get('parameters')
