4e2eeb6e4235e4ba93dd8b994b7f7e496ff64c22
redmindtech <69637152+redmindtech@users.noreply.github.com>
2020-10-01 18:17:59 +0530

    Update course.py

--- course.py
+++ course.py
@@ -79,18 +79,19 @@ def query(quiz):
         db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD,
                                       connection_timeout=60000)
         mycursor = db_connection.cursor()
-        string = """SELECT
-        questionsummary,
-        rightanswer
-        FROM x8ur_question_attempts 
-        WHERE questionusageid in (SELECT id from x8ur_quiz where name=%s);"""
+        string = """SELECT questionsummary, rightanswer FROM x8ur_question_attempts 
+                WHERE questionusageid in (SELECT id FROM `x8ur_quiz_attempts` WHERE quiz in (select id from x8ur_quiz where name = %s));"""
+        print("query--",string)
         mycursor.execute(string, (quiz,))
         Q = mycursor.fetchall()
         myList = []
         myList1 = []
+
         for row in Q:
+            print("row--",row)
             myList.append(row[0])
             myList1.append(row[1])
+            print("row completed")
         return myList, myList1
 
     except mysql.Error as err:
@@ -107,14 +108,13 @@ def main(quiz, i):
                                       connection_timeout=60000)
         print("Connected to:", db_connection.get_server_info())
         mycursor = db_connection.cursor()
-        string = """SELECT
-        questionsummary
-        FROM x8ur_question_attempts 
-        WHERE slot=%s AND questionusageid in (SELECT id from x8ur_quiz where name = %s);"""
+        string = """SELECT questionsummary FROM x8ur_question_attempts 
+        WHERE slot = %s AND questionusageid in (SELECT id FROM `x8ur_quiz_attempts` WHERE quiz in (select id from x8ur_quiz where name = %s))"""
+        print("query--", string)
         mycursor.execute(string, (i, str(quiz),))
         for g in mycursor:
             y = g
-            # print(y)
+            print(y)
         return y
     except mysql.Error as err:
         print(err)
@@ -142,10 +142,8 @@ def fetching(quiz):
                                       connection_timeout=60000)
         print("Connected to:", db_connection.get_server_info())
         mycursor = db_connection.cursor()
-        word = """SELECT 
-        questionsummary
-        FROM x8ur_question_attempts 
-        WHERE questionusageid in (SELECT uniqueid from x8ur_quiz_attempts where quiz in (SELECT id from x8ur_quiz where course in (SELECT courseid from x8ur_enrol where id in (SELECT enrolid from x8ur_user_enrolments where userid=%s))));"""
+        word = """SELECT questionsummary FROM x8ur_question_attempts 
+                WHERE questionusageid in (SELECT id FROM `x8ur_quiz_attempts` WHERE quiz in (select id from x8ur_quiz where name = %s));"""
         mycursor.execute(word, (quiz,))
         r = mycursor.fetchall()
         return r
@@ -194,6 +192,26 @@ def re_set():
     app.secret_score = 0
     app.email=0
 
+def getvideo(lesson):
+    try:
+        db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD,
+                                      connection_timeout=60000)
+        print("Connected to:", db_connection.get_server_info())
+        mycursor = db_connection.cursor()
+        tr = """select videolink, image from x8ur_video_image where lesson=%s""";
+        username = ''
+        mycursor.execute(tr, (lesson,))
+        row = mycursor.fetchone()
+
+        print("link--",row[0])
+        print('image--',row[1])
+        return row[0] , row[1]
+
+    except mysql.Error as err:
+        print(err)
+    print("Error Code:", err.errno)
+    print("SQLSTATE", err.sqlstate)
+    print("Message", err.msg)
 
 if __name__ == "__main__":
     main()
