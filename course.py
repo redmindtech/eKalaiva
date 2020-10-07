1ed33f0b8355469d6b43b9a6a51a5ee8e23d2283
redmindtech <69637152+redmindtech@users.noreply.github.com>
2020-10-07 15:00:52 +0530

    Update course.py

--- course.py
+++ course.py
@@ -44,18 +44,29 @@ def email(e_mail):
         print("Message", err.msg)
 
 
-def query2(questions):
+# def query2(questions):
+#     i = 0
+#     app.secret_key += 1
+#     i = app.secret_key
+#     if i < 5:
+#         que = ''.join(questions[i])
+#         return que
+#     else:
+#         end = app.secret_score
+#         return end
+
+def query2(questions, options):
     i = 0
     app.secret_key += 1
     i = app.secret_key
     if i < 5:
         que = ''.join(questions[i])
-        return que
+        return que, options[i]
     else:
         end = app.secret_score
-        return end
-
-
+        emptylist=[]
+        return end,emptylist
+    
 # def ques_count(quiz):
 #     try:
 #         db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD,connection_timeout=60000)
@@ -74,25 +85,40 @@ def query2(questions):
 #         print("SQLSTATE", err.sqlstate)
 #         print("Message", err.msg)
 
-def query(quiz):
-    try:
-        db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD,
-                                      connection_timeout=60000)
-        mycursor = db_connection.cursor()
-        string = """SELECT questionsummary, rightanswer FROM x8ur_question_attempts 
-                WHERE questionusageid in (SELECT id FROM `x8ur_quiz_attempts` WHERE quiz in (select id from x8ur_quiz where name = %s));"""
-        print("query--",string)
-        mycursor.execute(string, (quiz,))
-        Q = mycursor.fetchall()
-        myList = []
-        myList1 = []
+# def query(quiz):
+#     try:
+#         db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD,
+#                                       connection_timeout=60000)
+#         mycursor = db_connection.cursor()
+#         string = """SELECT questionsummary, rightanswer FROM x8ur_question_attempts 
+#                 WHERE questionusageid in (SELECT id FROM `x8ur_quiz_attempts` WHERE quiz in (select id from x8ur_quiz where name = %s));"""
+#         print("query--",string)
+#         mycursor.execute(string, (quiz,))
+#         Q = mycursor.fetchall()
+#         myList = []
+#         myList1 = []
 
-        for row in Q:
-            print("row--",row)
-            myList.append(row[0])
-            myList1.append(row[1])
-            print("row completed")
-        return myList, myList1
+#         for row in Q:
+#             print("row--",row)
+#             myList.append(row[0])
+#             myList1.append(row[1])
+#             print("row completed")
+#         return myList, myList1
+
+#     except mysql.Error as err:
+#         print(err)
+#         print("Error Code:", err.errno)
+#         print("SQLSTATE", err.sqlstate)
+#         print("Message", err.msg)
+
+def query(quiz):
+    try:       
+        queslist=[]
+        quesoptionlistoflist=[]
+        quesresultlist=[]
+        
+        queslist, quesoptionlistoflist, quesresultlist = quizdetails(quiz)
+        return queslist, quesoptionlistoflist, quesresultlist
 
     except mysql.Error as err:
         print(err)
@@ -101,21 +127,95 @@ def query(quiz):
         print("Message", err.msg)
 
 
+# def main(quiz, i):
+#     print(quiz)
+#     try:
+#         db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD,
+#                                       connection_timeout=60000)
+#         print("Connected to:", db_connection.get_server_info())
+#         mycursor = db_connection.cursor()
+#         string = """SELECT questionsummary FROM x8ur_question_attempts 
+#         WHERE slot = %s AND questionusageid in (SELECT id FROM `x8ur_quiz_attempts` WHERE quiz in (select id from x8ur_quiz where name = %s))"""
+#         print("query--", string)
+#         mycursor.execute(string, (i, str(quiz),))
+#         for g in mycursor:
+#             y = g
+#             print(y)
+#         return y
+#     except mysql.Error as err:
+#         print(err)
+#         print("Error Code:", err.errno)
+#         print("SQLSTATE", err.sqlstate)
+#         print("Message", err.msg)
+
 def main(quiz, i):
+    print(quiz)
+    try:       
+        queslist=[]
+        quesoptionlistoflist=[]
+        quesresultlist=[]
+        
+        queslist, quesoptionlistoflist, quesresultlist = quizdetails(quiz)
+        return queslist[0], quesoptionlistoflist[0]
+    except mysql.Error as err:
+        print(err)
+        print("Error Code:", err.errno)
+        print("SQLSTATE", err.sqlstate)
+        print("Message", err.msg)
+
+# For the input Quiz Lesson, the quizdetails method fetches all distinct 
+# Questions in a list, options for the all the questions in a list of lists, 
+# correct answers for the all questions in a list
+        
+def quizdetails(quiz):
     print(quiz)
     try:
         db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD,
                                       connection_timeout=60000)
         print("Connected to:", db_connection.get_server_info())
         mycursor = db_connection.cursor()
-        string = """SELECT questionsummary FROM x8ur_question_attempts 
-        WHERE slot = %s AND questionusageid in (SELECT id FROM `x8ur_quiz_attempts` WHERE quiz in (select id from x8ur_quiz where name = %s))"""
-        print("query--", string)
-        mycursor.execute(string, (i, str(quiz),))
-        for g in mycursor:
-            y = g
-            print(y)
-        return y
+        query = """select distinct ans.question, ques.questiontext from x8ur_question as ques 
+        JOIN x8ur_question_answers as ans ON ans.question = ques.id
+        JOIN x8ur_quiz_slots as slots ON ans.question = slots.questionid
+        JOIN x8ur_quiz as quiz ON quiz.id = slots.quizid
+        where quiz.name=%s order by ans.question"""
+        print("query--",query)
+        
+        mycursor.execute(query, (quiz,))
+        Q = mycursor.fetchall()
+        myQuesList = []
+        myOptionsList = []
+        myCorrectAnsList = []
+        myOptionsListofList=[]
+    
+        for row in Q:
+            #print("row--",row)
+            #print('row[1] question --', row[1])
+            myQuesList.append(row[1])
+            myOptionsList = []
+            
+            query1="""select distinct ans.answer, if(ans.fraction,ans.answer,0) correctans from x8ur_question as ques 
+            JOIN x8ur_question_answers as ans ON ans.question = ques.id
+            JOIN x8ur_quiz_slots as slots ON ans.question = %s
+            JOIN x8ur_quiz as quiz ON quiz.id = slots.quizid
+            where quiz.name=%s"""
+            #print("query1--",query1)
+
+            mycursor.execute(query1, (row[0],quiz))
+            optcur = mycursor.fetchall()
+            
+            for optrow in optcur:
+                myOptionsList.append(optrow[0])
+                if (optrow[1] !=  "0"):
+                    myCorrectAnsList.append(optrow[1])
+                    
+            myOptionsListofList.append(myOptionsList)
+
+        #print('MyQuesList --', myQuesList)
+        #print('myOptionsList --', myOptionsListofList)
+        #print('MyAnswerList --', myCorrectAnsList)
+        return myQuesList, myOptionsListofList, myCorrectAnsList
+    
     except mysql.Error as err:
         print(err)
         print("Error Code:", err.errno)
@@ -157,6 +257,7 @@ def fetching(quiz):
 def valid(ans, an, an1, an2, an3, answers):
     v = len(answers)
     print(v)
+    print ("secret code--", app.secret_code)
     j = app.secret_code
     print(j)
     crt = ''.join(answers[j])
@@ -176,13 +277,14 @@ def valid(ans, an, an1, an2, an3, answers):
         elif ans == crt:
             app.secret_score += 1
             reply = 'correct answer!!'
-            # printns(ans)
+            #print(ans)
         else:
             app.secret_score += 0
             reply = "Incorrect answer!! the correct answer is {}.".format(crt)
-            # print(reply)
-        # print(app.secret_score)
+        print("ans in course--", crt)
+        print("app.secret_score--",app.secret_score)
         app.secret_code += 1
+
         return reply, app.secret_score
 
 
